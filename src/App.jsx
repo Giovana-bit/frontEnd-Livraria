62eeb0de4f5c95ce2f325ed7bcbd0cee9c6e1091
Giovana <giovanadossantos107@gmail.com>
2025-09-01 22:19:27 -0300

    adicionando conexão com back-end

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,73 @@
-import React from "react";
-import Login from "./components/login";
+import React, { useState } from "react";
+import "./App.css";
+import api from "./services/api";
 
-function App() {
+function Login() {
+  // estados para armazenar os valores
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // função para capturar alterações no input
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "senha") {
+      setPassword(value);
+    }
+  };
+
+  // função de login
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const payload = { email, password };
+
+      const { data } = await api.post("/login", payload);
+
+      alert(data.response);
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.response);
+      }
+    }
+  };
+
   return (
-    <div>
-      <Login />
+    <div className="login-container">
+      <div className="login-box">
+        <img
+          src="https://github.com/Giovana-bit/Biblioteca-da-meia-noite-/blob/main/images/logo.png?raw=true"
+          alt="Logo"
+          className="login-logo"
+        />
+        <form onSubmit={handleLogin}>
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Informe seu email"
+            value={email}
+            onChange={handleChange}
+          />
+
+          <label>Senha:</label>
+          <input
+            type="password"
+            name="senha"
+            placeholder="Informe sua senha"
+            value={password}
+            onChange={handleChange}
+          />
+
+          <button type="submit">Acessar</button>
+        </form>
+        <p className="signup-text">
+          Não tem conta? <a href="#">Cadastre-se</a>
+        </p>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Login;
